e4a07e98d1bd52b9223dbf6b60bcc3b9f683639d
JuanPa-CT <juanptorres2003@gmail.com>
2025-03-26 08:57:25 -0500

    upload file

--- src/services/photoService.ts
+++ src/services/photoService.ts
@@ -2,21 +2,17 @@ import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
 
 export const takePhoto = async () => {
   try {
-    const isCameraAvailable = "Camera" in navigator;
-    await Camera.requestPermissions()
-    if (isCameraAvailable) {
-      const photo = await Camera.getPhoto({
-        resultType: CameraResultType.Uri,
-        source: CameraSource.Prompt,
-        quality: 90,
-      });
-      const webPath = photo?.webPath;
-      const blob = await (await fetch(`${webPath}`)).blob();
-      const fileName = `photo.${photo.format || "jpg"}`;
-      const file = new File([blob], fileName)
+    const photo = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Prompt,
+      quality: 90,
+    });
+    const webPath = photo?.webPath;
+    const blob = await (await fetch(`${webPath}`)).blob();
+    const fileName = `photo.${photo.format || "jpg"}`;
+    const file = new File([blob], fileName)
 
-      return { path: file, format: photo.format };
-    }
+    return { path: file, format: photo.format };
   } catch (error) {
     console.error("Error al tomar la foto:", error);
     return null;
