38b1eeaf74b153c7a59755f35eced3114a1277d2
JuanPa-CT <juanptorres2003@gmail.com>
2025-03-26 08:45:01 -0500

    upload file

--- src/services/photoService.ts
+++ src/services/photoService.ts
@@ -3,29 +3,19 @@ import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
 export const takePhoto = async () => {
   try {
     const isCameraAvailable = "Camera" in navigator;
-
+    await Camera.requestPermissions()
     if (isCameraAvailable) {
       const photo = await Camera.getPhoto({
         resultType: CameraResultType.Uri,
         source: CameraSource.Prompt,
         quality: 90,
       });
-      if (photo.webPath) {
+      const webPath = photo?.webPath;
+      const blob = await (await fetch(`${webPath}`)).blob();
+      const fileName = `photo.${photo.format || "jpg"}`;
+      const file = new File([blob], fileName)
 
-        const response = await fetch(photo?.webPath);
-        const blob = await response.blob();
-
-        const formatImage = `image/${photo.format}`
-        return {
-          path: new File([blob], `photo.${photo.format}`, {
-            type: blob.type || formatImage,
-            lastModified: Date.now(),
-          }), format: formatImage
-        }
-      }
-      return null
-    } else {
-      return await selectFileFromInput();
+      return { path: file, format: photo.format };
     }
   } catch (error) {
     console.error("Error al tomar la foto:", error);
@@ -33,24 +23,3 @@ export const takePhoto = async () => {
   }
 };
 
-const selectFileFromInput = (): Promise<{ path: File; format: string } | null> => {
-  return new Promise((resolve) => {
-    const input = document.createElement("input");
-    input.type = "file";
-    input.accept = "image/*";
-    input.style.display = "none";
-
-    input.addEventListener("change", (event: Event) => {
-      const file = (event.target as HTMLInputElement).files?.[0];
-      if (file) {
-        resolve({ path: file, format: file.type.split("/")[1] });
-      } else {
-        resolve(null);
-      }
-    });
-
-    document.body.appendChild(input);
-    input.click();
-    document.body.removeChild(input);
-  });
-};
