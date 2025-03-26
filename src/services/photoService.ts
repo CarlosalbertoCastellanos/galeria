025d781a31c5e47bef8a28f89c9097ed1c42a596
JuanPa-CT <juanptorres2003@gmail.com>
2025-03-26 09:19:44 -0500

    upload file

--- src/services/photoService.ts
+++ src/services/photoService.ts
@@ -1,4 +1,5 @@
 import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
+import { Filesystem, Directory } from '@capacitor/filesystem';
 
 export const takePhoto = async () => {
   try {
@@ -7,10 +8,22 @@ export const takePhoto = async () => {
       source: CameraSource.Prompt,
       quality: 90,
     });
+
     const webPath = photo?.webPath;
-    const blob = await (await fetch(`${webPath}`)).blob();
-    const fileName = `photo.${photo.format || "jpg"}`;
-    const file = new File([blob], fileName)
+    const readFile = await Filesystem.readFile({
+      path: `${photo.path}`,
+    });
+
+    const byteCharacters = atob(readFile.data as string);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+
+    const blob = new Blob([byteArray], { type: `image/${photo.format}` });
+
+    const file = new File([blob], `photo.${photo.format}`, { type: `image/${photo.format}` });
 
     return { path: file, format: photo.format };
   } catch (error) {
